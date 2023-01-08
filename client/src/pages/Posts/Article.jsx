5fe77d394cdbcfa7bc958a7200380f1803c8cee0
cauefrancisco <cauefranscis@gmail.com>
2023-01-08 14:35:03 -0300

    update in dev pages

--- client/src/pages/Posts/Article.jsx
+++ client/src/pages/Posts/Article.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { AiFillEdit, AiOutlineDelete } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
+import Menu from '../../components/Menu';
 
 const Article = () => {
   return (
     <div className='article'>
       <div className="content">
-
+        
         <img src="https://i0.wp.com/www.michigandaily.com/wp-content/uploads/2021/04/bojack-1.jpg?fit=2400%2C1600&ssl=1" alt="" />
         <div className="user">
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9L1QCer3D9wTWXkqIVZ076NAux-h64jSiWw&usqp=CAU" alt="" />
@@ -23,7 +24,11 @@ const Article = () => {
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae aut, maiores animi perspiciatis iste veniam, perferendis nam ipsum saepe eveniet dignissimos sunt, adipisci voluptatem laboriosam consequuntur dolor! Voluptate, dignissimos facere? Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis odit exercitationem libero necessitatibus modi, magnam porro illo! Officiis, dicta facere eius veniam optio pariatur! Odit alias repellendus distinctio hic minus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Et omnis sint nulla reiciendis doloribus inventore hic facere iste ea, recusandae totam dolores minima minus quaerat sapiente magnam sit odio ipsum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus animi ratione id ea sequi, consequuntur a qui fugit nisi aliquid deleniti magni illum optio culpa. Unde repudiandae fugit corporis cumque. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae aut, maiores animi perspiciatis iste veniam, perferendis nam ipsum saepe eveniet dignissimos sunt, adipisci voluptatem laboriosam consequuntur dolor! Voluptate, dignissimos facere? Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis odit exercitationem libero necessitatibus modi, magnam porro illo! Officiis, dicta facere eius veniam optio pariatur! Odit alias repellendus distinctio hic minus? Lorem ipsum dolor sit amet consectetur adipisicing elit. Et omnis sint nulla reiciendis doloribus inventore hic facere iste ea, recusandae totam dolores minima minus quaerat sapiente magnam sit odio ipsum. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus animi ratione id ea sequi, consequuntur a qui fugit nisi aliquid deleniti magni illum optio culpa. Unde repudiandae fugit corporis cumque. </p>
       
       </div>
-      <div className="menu"></div>
+      
+      <div className="menu-section">
+      <h1>Other posts you might like</h1>
+      <Menu />
+      </div>
     </div>
   )
 }
--- client/src/pages/Posts/CreateArticle.jsx
+++ client/src/pages/Posts/CreateArticle.jsx
@@ -1,8 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 const CreateArticle = () => {
+  const [value, setValue] = useState('');
+  const category = [
+    {
+      id: 1,
+      name: "tech",
+    },
+    {
+      id: 2,
+      name: "culture",
+    },
+    {
+      id: 3,
+      name: "food",
+    },
+  ];
+
+  // const [categories, setCategories] = useState([]);
+
+  // const addCategory = (name) => {
+  //   categories = category;
+
+  //   if(!categories.hasOwnProperty(name)){
+  //     setCategories(categories.push(name));
+  //   }
+  // };
+
   return (
-    <div>CreateArticle</div>
+    <div className='addArticle'>
+      <div className="content">
+        <input type="text" placeholder='Title' />
+        <div className="editorContainer">
+         <ReactQuill theme="snow"  value={value} onChange={setValue} className="quill" />
+        </div>
+      </div>
+      <div className="menu">
+        <div className="item">
+          <h1>Publish</h1>
+          <span>
+            <b> Satatus: </b> Draft 
+          </span>
+          <span>
+            <b> Visibility: </b> Public 
+          </span>
+          <label htmlFor="file" >Upload image</label>
+          <input type="file" id='file' style={{display:'none'}}/>
+          <div className="buttons">
+          <button>Save as a draft</button>
+          <button>Update</button>
+          </div>
+        </div>
+        <div className="item">
+          <h1>Category</h1>
+          {category.map((cat) =>(
+            <>
+            <input key={cat.id} type="checkbox" name='category' value={cat.name} />
+            <label htmlFor='categories'>{cat.name}</label>
+            </>
+          ))}
+        </div>
+      </div>
+    </div>
   )
 }
 
